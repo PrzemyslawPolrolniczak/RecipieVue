b098d11175a33bad87f3f1aefc7c881056a4cbb8
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2020-01-13 23:39:32 +0100

    close #1 - add tailwind and finish setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueCompositionApi from "@vue/composition-api";
+import "./assets/tailwind.css";
 
 Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
