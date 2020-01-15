513651448563789c4d134bedf9d72fe1aa6d66a1
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2020-01-15 22:49:09 +0100

    feat: header container + filters + quicksearch

---
+++ src/components/TheHeader/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header flex px-4 items-center justify-between">
+    <div>
+      <h2 class="text-3xl">Przepiśnik</h2>
+    </div>
+    <div class="flex">
+      <nav>
+        <ul class="flex">
+          <li class="text-white mx-2 text-xl">Śniadania</li>
+          <li class="text-white mx-2 text-xl">Obiady</li>
+          <li class="text-white mx-2 text-xl">Kolacje</li>
+        </ul>
+      </nav>
+      <search-input additionalClasses="bg-gray-600" />
+    </div>
+  </header>
+</template>
+
+<script>
+import SearchInput from "../SearchInput/SearchInput.vue";
+export default {
+  name: "TheHeader",
+  components: { SearchInput }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 80px;
+}
+</style>
